60cd870e720f710df2d6ee5f5e6742bf8930a9c5
bencematyasi <31878635+bencematyasi@users.noreply.github.com>
2019-05-06 17:53:18 +0200

    menu item design so basic, some changes in code, main acitivity design, recyclerview added etc

--- app/src/main/java/com/exam/forumproject/GUI/MainActivity.java
+++ app/src/main/java/com/exam/forumproject/GUI/MainActivity.java
@@ -1,10 +1,16 @@
 package com.exam.forumproject.GUI;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +18,12 @@ import android.view.MenuItem;
 import com.exam.forumproject.R;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    RecyclerViewAdapter adapter;
+    Context ctx;
+
+    RecyclerView postListView;
+    FloatingActionButton newPostBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +31,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ctx = this.getBaseContext();
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        postListView = findViewById(R.id.recyclerView);
+
+        newPostBtn = findViewById(R.id.fab);
+        newPostBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent inten = new Intent(ctx, NewPostActivity.class);
+                startActivity(inten);
             }
         });
     }
@@ -50,5 +65,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private void initRecyclerView() {
+        Log.d(TAG, "initRecyclerView: init RecyclerView.");
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        //adapter = new RecyclerViewAdapter(this, model.getAll());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/com/exam/forumproject/GUI/NewPostActivity.java
+++ app/src/main/java/com/exam/forumproject/GUI/NewPostActivity.java
@@ -2,14 +2,25 @@ package com.exam.forumproject.GUI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.exam.forumproject.R;
 
 public class NewPostActivity extends AppCompatActivity {
 
+    Button btnPost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
+
+        btnPost = findViewById(R.id.btnPost);
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
